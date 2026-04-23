46bf4d0725fda48e5032bd8741e0a5e6f84c6a20
Ifeoluwa Aluko <74814970+Ifecode@users.noreply.github.com>
2026-04-23 15:21:16 +0100

    Migration

---
+++ settings_app/migrations/0005_merge_encrypted_names_and_gmail_oauth2.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('settings_app', '0003_state_only_encrypted_field_names'),
+        ('settings_app', '0004_integrationconfig_gmail_oauth2'),
+    ]
+
+    operations = []
