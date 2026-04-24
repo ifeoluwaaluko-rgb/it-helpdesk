5e981ce2c1e1814d4ca85aa04eac0f057399cbde
Ifeoluwa Aluko <74814970+Ifecode@users.noreply.github.com>
2026-04-24 16:55:00 +0100

    Fix

--- settings_app/migrations/0006_merge_use_ssl_and_existing_merge.py
+++ settings_app/migrations/0006_merge_use_ssl_and_existing_merge.py
@@ -4,7 +4,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("settings_app", "0004_integrationconfig_use_ssl"),
+        ("settings_app", "0003_integrationconfig_use_ssl"),
         ("settings_app", "0005_merge_encrypted_names_and_gmail_oauth2"),
     ]
 
